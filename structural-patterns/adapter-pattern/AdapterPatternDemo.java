73d3ac41cd8a34d4a1ba9a1bddfe52f7e957ba76
21120447 <haonhat2729@gmail.com>
2024-08-07 12:50:36 +0700

    feat: add adapter  patterns docs

---
+++ structural-patterns/adapter-pattern/AdapterPatternDemo.java
@@ -0,0 +1,6 @@
+public class AdapterPatternDemo {
+    public static void main(String[] args) {
+        VietnameseTarget client = new TranslatorAdapter(new JapaneseAdaptee());
+        client.send("Xin chào");
+    }
+}
